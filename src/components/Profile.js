44de3e890c3b7165cf84398e4bd2edb91df0c442
RedSm1le <80716303+RedSm1le@users.noreply.github.com>
2021-03-15 23:17:33 +0200

    init project gethub

---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img
+                    src="https://builtin.com/sites/default/files/styles/ckeditor_optimize/public/inline-images/functional-new-programming-languages.jpg"/>
+            </div>
+            <div>ava + description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile
